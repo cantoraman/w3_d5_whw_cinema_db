b7c231037f7dec1058ed5111863fc5c5e2d830f4
Can Toraman <cantoraman@gmail.com>
2018-06-08 14:14:59 +0100

    customer delete all applies

--- db/console.rb
+++ db/console.rb
@@ -3,6 +3,7 @@ require_relative('../models/customer')
 require_relative('../models/ticket')
 require_relative('../models/film')
 
+Customer.delete_all()
 
 customer1 = Customer.new ({'name' => "Can", 'age' => "33"})
 customer2 = Customer.new ({'name' => "John", 'age' => "400"})
